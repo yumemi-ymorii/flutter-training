0bf1ccf4ee36491ea4c87a9a7f40ddbec0205600
yumemi-ymorii <y_morii@yumemi.co.jp>
2024-04-24 09:45:21 +0900

    Widget作成&配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,118 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SizedBox.expand(
+          child: FractionallySizedBox(
+            widthFactor: 0.5,
+            alignment: FractionalOffset.center,
+            child: Column(
+              children: <Widget>[
+                // Spacer, WeatherTmp, Flexibleで位置調整
+                const Spacer(),
+                _WeatherTmp(),
+                Flexible(
+                  child: Column(
+                    children: [
+                      // 80pixcelの間隔確保
+                      const SizedBox(height: 80),
+                      _Buttons(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+}
+
+class _WeatherTmp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
+    final logoWidth = screenSize.width * 0.5;
+    final textWidth = logoWidth * 0.5;
+
+    final theme = Theme.of(context);
+
+    final placeholder = SizedBox.square(
+      dimension: logoWidth,
+      child: const Placeholder(),
+    );
+
+    final tempTextGroup = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: textWidth,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Align(
+              child: Text(
+                '**℃',
+                style: theme.textTheme.labelLarge!
+                    .merge(const TextStyle(color: Colors.blue)),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: textWidth,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Align(
+              child: Text(
+                '**℃',
+                style: theme.textTheme.labelLarge!
+                    .merge(const TextStyle(color: Colors.red)),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [placeholder, tempTextGroup],
+    );
+  }
+}
+
+class _Buttons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
+    final logoWidth = screenSize.width * 0.5;
+    final textWidth = logoWidth * 0.5;
+
+    final buttonGroup = Row(
+      children: [
+        SizedBox(
+          width: textWidth,
+          child: Align(
+            child: TextButton(
+              onPressed: () {},
+              child: const Text('Close'),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: textWidth,
+          child: Align(
+            child: TextButton(
+              onPressed: () {},
+              child: const Text('Reload'),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    return buttonGroup;
   }
 }
