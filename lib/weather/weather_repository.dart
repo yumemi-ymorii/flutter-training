b261b301c77c64f8f150fa57527a87a5f48087d4
yumemi-ymorii <y_morii@yumemi.co.jp>
2024-05-30 10:57:25 +0900

    RepositryにProviderを作成

--- lib/weather/weather_repository.dart
+++ lib/weather/weather_repository.dart
@@ -3,7 +3,16 @@ import 'dart:convert';
 import 'package:flutter_training/weather/data/weather_exception.dart';
 import 'package:flutter_training/weather/location.dart';
 import 'package:flutter_training/weather/weather.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
+
+part 'weather_repository.g.dart';
+
+@riverpod
+WeatherRepository weatherRepository(WeatherRepositoryRef ref) {
+  final yumemiWeather = YumemiWeather();
+  return WeatherRepository(weatherApi: yumemiWeather);
+}
 
 class WeatherRepository {
   WeatherRepository({required YumemiWeather weatherApi})
