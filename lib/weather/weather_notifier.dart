6e04f54554459e3ac753b3d4f8263c034b03dd7e
yumemi-ymorii <y_morii@yumemi.co.jp>
2024-05-30 10:31:43 +0900

    NotifierからRepositryのメソッドを呼び出せるように変更

--- lib/weather/weather_notifier.dart
+++ lib/weather/weather_notifier.dart
@@ -1,16 +1,27 @@
+import 'package:flutter_training/weather/location.dart';
 import 'package:flutter_training/weather/weather.dart';
+import 'package:flutter_training/weather/weather_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
 
 part 'weather_notifier.g.dart';
 
 @riverpod
 class WeatherNotifier extends _$WeatherNotifier {
+  final _yumemiWeather = YumemiWeather();
+  late final _weatherRepositry = WeatherRepository(weatherApi: _yumemiWeather);
+
   @override
   Weather? build() {
     return null;
   }
 
   void update(Weather weather) {
+    state = weather;
+  }
+
+  void fetchWeather(Location location) {
+    final weather = _weatherRepositry.fetchWeather(location);
     state = weather;
   }
 }
